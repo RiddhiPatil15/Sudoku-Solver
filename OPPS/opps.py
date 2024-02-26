03532921b275f5d0df55463b26af9bd63f9a7c40
RiddhiPatil15 <riddhipatilofficial15@gmail.com>
2024-02-26 18:00:42 +0530

    __init__ constructors

--- OPPS/opps.py
+++ OPPS/opps.py
@@ -1,17 +1,15 @@
 class Item:
-    def __init__(self, name):
+    def __init__(self, name, price, quantity):
         print(f"An instance created: {name}")
-    def calculate_total_price(self, x, y):
-        return x*y
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
-item1 = Item("Phone")
-item1.name ='Phone'
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("Phone", 100, 5)
+print(item1.calculate_total_price())
 
-item2 = Item("Laptop")
-item2.name ='Laptop'
-item2.price = 1500
-item2.quantity = 4
-print(item2.calculate_total_price(item2.price, item2.quantity))
+item2 = Item("Laptop", 1000, 3)
+print(item2.calculate_total_price())
+
